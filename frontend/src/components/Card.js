81ee82275c9957b3077c53cc8f706b0ccc249b07
ZejusJS <pepitopoky.jp@gmail.com>
2023-01-02 21:21:44 +0100

    more reusable card

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,31 +1,30 @@
-import CardImg from '../images/katie-zaferes.png'
 import Star from '../images/star.svg'
 
-const Card = () => {
+const Card = ({ img, title, rating, ratingCount, country, price, per }) => {
     return (
         <div className="card">
             <div className="card-image">
                 <span className="card-status">SOLD OUT</span>
-                <img src={CardImg} alt="" />
+                <img src={img} alt="" />
             </div>
             <div className="card-info">
                 <div className='card-info-1'>
                     <span className='card-rating-con'>
                         <img src={Star} alt="" className='card-star' />
-                        <span className='card-rating'>5.0</span>
-                        <span className='card-num-rating muted'>(6)</span>
+                        <span className='card-rating'>{rating}</span>
+                        <span className='card-num-rating muted'>({ratingCount})</span>
                     </span>
                     <span className='muted'>•</span>
-                    <span className='muted'>USA</span>
+                    <span className='muted'>{country}</span>
                 </div>
                 <div className='card-info-2'>
-                    <p className='card-title'>Life lessons with Katie Zaferes</p>
+                    <p className='card-title'>{title}</p>
                 </div>
                 <div className='card-info-3'>
                     <span className='bold'>From </span>
-                    <span className='bold'>$136 </span>
+                    <span className='bold'>${price} </span>
                     <span className='muted'>/ </span>
-                    <span className='muted'>person</span>
+                    <span className='muted'>{per}</span>
                 </div>
             </div>
         </div>
--- frontend/src/components/Hero.js
+++ frontend/src/components/Hero.js
@@ -1,16 +1,36 @@
 import React from 'react'
 import PhotoGrid from '../images/photo-grid.png'
 import Card from './Card'
+import CardImg from '../images/katie-zaferes.png'
+
+const cardDetails = {
+    img: CardImg,
+    title: 'Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.',
+    rating: '5.0',
+    ratingCount: 6,
+    country: 'USA',
+    price: 136,
+    per: 'person'
+}
 
 const Hero = () => {
+    const { title, img, rating, ratingCount, country, price, per } = cardDetails
     return (
         <section className='hero'>
             <img src={PhotoGrid} alt="" className='photo-grid' />
             <h2 className='hero-header'>Online Experiences</h2>
             <p className='hero-paragraph'>
-                Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
+                {title}
             </p>
-            <Card />
+            <Card
+                img={img}
+                title={title}
+                rating={rating}
+                ratingCount={ratingCount}
+                country={country}
+                price={price}
+                per={per}
+            />
         </section>
     )
 }
